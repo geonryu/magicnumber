25f0af7528bcc4c8b68293e82e70f84cb632751a
geonryu <60060831+geonryu@users.noreply.github.com>
2023-09-15 20:31:59 +0900

    아이디찾기
    페이지추가

--- public/js/join.js
+++ public/js/join.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const $joinId = document.getElementById("joinId");
+  const $joinName = document.getElementById("joinName");
+  const $joinSoNum = document.getElementById("joinSoNum");
+  const $joinGender = document.getElementById("joinGender");
+  const $joinCarrier = document.getElementById("joinCarrier");
+  const $joinTel = document.getElementById("joinTel");
+  const $verifyCallBtn = document.getElementById("verifyCall");
+  const $joinNickName = document.getElementById("joinNickName");
+  const $joinPw = document.getElementById("joinPw");
+  const $joinPwCk = document.getElementById("joinPwCk");
+
   const $selects = document.querySelectorAll(".policy .allow-list div input");
   const $selectAll = document.getElementById("allowAll");
 
@@ -12,6 +23,89 @@ document.addEventListener("DOMContentLoaded", () => {
       // nextBtns[0].disabled = true;
     }
   };
+  const checkTypedAuth = () => {
+    const joinName = $joinName.value;
+    const joinTel = $joinTel.value;
+    const joinTelInput = joinTel.slice(4);
+
+    const userEmail = $joinId.value;
+    const joinCarrier = $joinCarrier.value;
+    const joinSoNum = $joinSoNum.value;
+    const joinGender = $joinGender.value;
+    const re = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/; //email 유효성검사
+    if (
+      joinName &&
+      joinTelInput.length >= 8 &&
+      joinTel.replace(/-/g, "").length >= 10 &&
+      (userEmail !== "" || !re.test(userEmail)) &&
+      joinSoNum.length === 8 &&
+      joinCarrier !== "default" &&
+      joinGender !== "default"
+    ) {
+      document.getElementById("verifyCall").removeAttribute("disabled");
+      document.getElementById("verifyCall").classList.remove("bg-bgDisabled");
+    } else {
+      document.getElementById("verifyCall").setAttribute("disabled", "");
+      document.getElementById("verifyCall").classList.add("bg-bgDisabled");
+    }
+  };
+
+  $joinName.addEventListener("keyup", checkTypedAuth);
+
+  $joinTel.addEventListener("keyup", (e) => {
+    e.target.value = e.target.value
+      .replace(/[^0-9]/g, "")
+      .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
+      .replace(/(\-{1,2})$/g, "");
+    // 하이픈 자동 삽입 정규식
+    checkTypedAuth();
+  });
+
+  const re = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+  $joinId.addEventListener("keyup", (e) => {
+    const userEmail = $joinId.value;
+    const messageWarn = document.getElementById("emailWarn");
+    if (userEmail == "" || !re.test(userEmail)) {
+      messageWarn.style.display = "block";
+      return false;
+    } else {
+      messageWarn.style.display = "none";
+    }
+  });
+  // // 이메일 유효성 검사 끝
+
+  $joinPw.addEventListener("keyup", (e) => {
+    const userPW = $joinPw.value;
+    const ckInc = document.querySelectorAll(".ck-pw-eff span");
+
+    const num = userPW.search(/[0-9]/g);
+    const eng = userPW.search(/[a-z]/gi);
+    const spe = userPW.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+    userPW.length < 8 || userPW.length > 20
+      ? (ckInc[3].style.color = "#CCCCCC")
+      : (ckInc[3].style.color = "#EB5602");
+    eng < 0
+      ? (ckInc[0].style.color = "#CCCCCC")
+      : (ckInc[0].style.color = "#EB5602");
+    num < 0
+      ? (ckInc[1].style.color = "#CCCCCC")
+      : (ckInc[1].style.color = "#EB5602");
+    spe < 0
+      ? (ckInc[2].style.color = "#CCCCCC")
+      : (ckInc[2].style.color = "#EB5602");
+  });
+
+  $joinPwCk.addEventListener("keyup", (e) => {
+    const currentInput = $joinPw.value;
+    const userRepeat = $joinPwCk.value;
+
+    const $msgWarn = document.getElementById("pwWarn");
+    currentInput === userRepeat
+      ? ($msgWarn.style.display = "none")
+      : ($msgWarn.style.display = "block");
+  });
+  // pw유효성 끝
 
   $selectAll.addEventListener("change", (e) => {
     let isAll = e.target.checked;
@@ -36,50 +130,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   // 약관 전체 동의 끝
-
-  const re = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-
-  document.getElementById("joinId").addEventListener("keyup", (e) => {
-    const userEmail = document.getElementById("joinId").value;
-    const messageWarn = document.getElementById("emailWarn");
-    if (userEmail == "" || !re.test(userEmail)) {
-      messageWarn.style.display = "block";
-      return false;
-    } else {
-      messageWarn.style.display = "none";
-    }
-  });
-  // // 이메일 유효성 검사 끝
-
-  document.getElementById("joinPw").addEventListener("keyup", (e) => {
-    const userPW = document.querySelector("#joinPw").value;
-    const ckInc = document.querySelectorAll(".ck-pw-eff span");
-
-    const num = userPW.search(/[0-9]/g);
-    const eng = userPW.search(/[a-z]/gi);
-    const spe = userPW.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
-    userPW.length < 8 || userPW.length > 20
-      ? (ckInc[3].style.color = "#CCCCCC")
-      : (ckInc[3].style.color = "#EB5602");
-    eng < 0
-      ? (ckInc[0].style.color = "#CCCCCC")
-      : (ckInc[0].style.color = "#EB5602");
-    num < 1
-      ? (ckInc[1].style.color = "#CCCCCC")
-      : (ckInc[1].style.color = "#EB5602");
-    spe < 2
-      ? (ckInc[2].style.color = "#CCCCCC")
-      : (ckInc[2].style.color = "#EB5602");
-  });
-
-  document.getElementById("joinPwCk").addEventListener("keyup", (e) => {
-    const currentInput = document.getElementById("joinPw").value;
-    const userRepeat = document.getElementById("joinPwCk").value;
-
-    const $msgWarn = document.getElementById("pwWarn");
-    currentInput === userRepeat
-      ? ($msgWarn.style.display = "none")
-      : ($msgWarn.style.display = "block");
-  });
-  // pw유효성 끝
 });
