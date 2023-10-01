fe06803b22e22fb7b798b19e72dcdb99c6c69f12
SDC <sdcomms4227@gmail.com>
2023-10-01 18:22:19 +0900

    4. 상담 API 처리 수정

--- middleware/counselor.js
+++ middleware/counselor.js
@@ -1,54 +1,8 @@
 const axios = require("axios");
 
+// 4. 상담 - 상담사, 상담내역
 const counselor = {
-  // 1. 상담사 목록
-  async getCounselorList(params) {
-    console.log("getCounselorList(params)", params);
-
-    try {
-      // API URL
-      const apiUrl = "/api/v1/counselor";
-
-      // Axios 인스턴스 생성
-      const axiosInstance = axios.create({
-        baseURL: "http://api.magicnumber.co.kr", // API 기본 호스트 URL
-        headers: {
-          "content-Type": "application/json",
-          authorization: "",
-        }, // 헤더 설정
-      });
-
-      // GET 요청 보내기
-      const response = await axiosInstance.post(apiUrl, params);
-
-      const responseStatus = response.status;
-      const responseStatusText = response.statusText;
-      const responseData = response.data;
-
-      // console.log("=== success start ===========================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== success end ============================");
-
-      // 목록 반환
-      return responseData;
-    } catch (error) {
-      const responseStatus = error.response.status;
-      const responseStatusText = error.response.statusText;
-      const responseData = error.response.data;
-
-      // console.log("=== error start =============================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== error end ==============================");
-
-      return responseData;
-    }
-  },
-
-  // 2. 상담사 정보
+  // 4.1 [POST] 상담사 목록 전체 호출
   async getCounselor(params) {
     console.log("getCounselor(params)", params);
 
@@ -72,40 +26,22 @@ const counselor = {
       const responseStatusText = response.statusText;
       const responseData = response.data;
 
-      // console.log("=== success start ===========================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== success end ============================");
-
-      // 상담사 정보
-      const counselorInfo = responseData.result.length > 0 ? responseData.result[0] : "";
-      // console.log('counselorInfo', counselorInfo);
-
-      return counselorInfo;
+      // 목록 반환
+      return responseData;
     } catch (error) {
       const responseStatus = error.response.status;
       const responseStatusText = error.response.statusText;
       const responseData = error.response.data;
 
-      // console.log("=== error start =============================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== error end ==============================");
-
       return responseData;
     }
   },
 
-  // 3. 후기 목록
-  async getReviewList(csrid, accessToken) {
-    console.log("getReviewList(csrid)", csrid, accessToken);
+  // 4.2 [GET] 상담사 개별 리뷰 호출
+  async getCounselorReview(params, accessToken) {
+    console.log("getCounselorReview(params, accessToken)", params, accessToken);
 
     try {
-      // 20230910 테스트용 설정
-      csrid = csrid > 0 ? csrid : 12705;
-
       // API URL
       const apiUrl = "/api/v1/counselor/review/csr";
 
@@ -118,11 +54,6 @@ const counselor = {
         }, // 헤더 설정
       });
 
-      // 요청 파라미터 설정
-      const params = {
-        csrid: csrid,
-      };
-
       // GET 요청 보내기
       const response = await axiosInstance.get(apiUrl, { params: params });
 
@@ -130,30 +61,60 @@ const counselor = {
       const responseStatusText = response.statusText;
       const responseData = response.data;
 
-      // console.log("=== success start ===========================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== success end ============================");
-
-      // 후기 목록
-      const reviewList = responseData.result;
-      // console.log("reviewList", reviewList);
-
-      return reviewList;
+      return responseData;
     } catch (error) {
+      console.log(error);
       const responseStatus = error.response.status;
       const responseStatusText = error.response.statusText;
       const responseData = error.response.data;
 
-      // console.log("=== error start =============================");
-      // console.log(responseStatus);
-      // console.log(responseStatusText);
-      // console.log(responseData);
-      // console.log("=== error end ==============================");
-
       return responseData;
     }
+  },
+
+  // 4.3 [PUT] 멘트 없이 전화걸기 설정
+  async sendCallWithoutComment(params) {
+    console.log("sendCallWithoutComment(params)", params);
+    try {
+      // API URL
+      const apiurl = "/api/v1/counselor";
+    } catch (error) {}
+  },
+
+  // 4.4 [GET] 상담내역 목록 호출
+  async getMyCounselingHistory(params) {
+    console.log("getMyCounselingHistory(params)", params);
+    try {
+      // API URL
+      const apiurl = "/api/v1/counselor/history";
+    } catch (error) {}
+  },
+
+  // 4.5 [GET] 리뷰 목록 호출
+  async getMyReview(params) {
+    console.log("getMyReview(params)", params);
+    try {
+      // API URL
+      const apiurl = "/api/v1/counselor/review";
+    } catch (error) {}
+  },
+
+  // 4.6 [POST] 리뷰 작성
+  async createReview(params) {
+    console.log("createReview(params)", params);
+    try {
+      // API URL
+      const apiurl = "/api/v1/counselor/review";
+    } catch (error) {}
+  },
+
+  // 4.7 [PUT] 리뷰 수정
+  async updateReview(params) {
+    console.log("updateReview(params)", params);
+    try {
+      // API URL
+      const apiurl = "/api/v1/counselor/review";
+    } catch (error) {}
   },
 };
 
